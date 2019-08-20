d7bdc0b653dab5a65286e06ffb4fdfff8f7cdf74
krishna <chaitanyaradon89@gmail.com>
2019-08-20 10:19:04 -0600

    Tuesday Morning

--- bin_to_array.py
+++ bin_to_array.py
@@ -2,5 +2,5 @@ import numpy as np
 
 if __name__ == "__main__":
     data_type = np.dtype(np.uint16).newbyteorder('>')
-    cmos_array = np.fromfile("bin_files/P1_E2_T2__marks.bin", dtype=data_type)
+    cmos_array = np.fromfile("bin_files/P1_E3_T1__marks.bin", dtype=data_type)
     print(cmos_array)
