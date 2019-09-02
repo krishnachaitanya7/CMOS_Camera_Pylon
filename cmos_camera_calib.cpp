1e827165e99d6cfb9fd4bf702176cc107fa01137
kodur krishna chaitanya <chaitanyaradon89@gmail.com>
2019-09-02 15:01:39 -0600

    Good progress

---
+++ cmos_camera_calib.cpp
@@ -0,0 +1,10 @@
+//
+// Created by shine on 9/2/19.
+//
+/*
+ * Goo Resources for camera Calibration
+ * http://www.cs.cmu.edu/~16385/s17/Slides/11.1_Camera_matrix.pdf
+ * Entire slides: http://www.cs.cmu.edu/~16385/s17/Slides/
+ * */
+
+
